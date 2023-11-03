8eea739c8a216d4041c86a65c48a8582d398faa2
Miles Burke <milesj76@gmail.com>
2023-11-03 18:40:30 -0400

    add tailwind

--- analogy-quest/src/routes/+layout.svelte
+++ analogy-quest/src/routes/+layout.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
+    import "../app.css"
     import type { LayoutData } from './$types';
     
     export let data: LayoutData;
 </script>
 
-<div>
+<div class="">
     <nav>
-        
+        <a href="/">Analogy Quest</a>
+
+        <div>
+            <a href="/login">Sign In</a>
+        </div>
     </nav>
 
     <slot />
 </div>
+
+<style>
+    nav {
+        @apply w-full bg-slate-200 flex justify-between py-4 px-8;
+    }
+</style>
