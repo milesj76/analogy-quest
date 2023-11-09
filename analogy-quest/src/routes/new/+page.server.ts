f963ad8a0434dd84974c2c7f7c9e97236389ae05
Miles Burke <milesj76@gmail.com>
2023-11-09 00:02:48 -0500

    add create analogy

--- analogy-quest/src/routes/new/+page.server.ts
+++ analogy-quest/src/routes/new/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
     if (!locals.pb.authStore.isValid) {
@@ -8,3 +8,51 @@ export const load = (async ({ locals }) => {
 
     return {};
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+    default: async ({ request, locals }) => {
+        const body = Object.fromEntries(await request.formData())
+        const { topic, analogy } = body
+
+        console.log('topic:', topic)
+
+        let id
+
+        // TODO: add validation to form
+
+        // handle new topic
+        try {
+            const topicExists = await locals.pb.collection('topics').getFirstListItem(`name="${topic}"`)
+            console.log(topicExists, 'found in topics!')
+            id = topicExists.id
+        } catch (error) {
+            // console.log(error)
+            console.log(topic, "doesn't exist! Adding to topics.")
+            const res = await locals.pb.collection('topics').create({ "name": topic })
+            id = res.id
+        }
+
+        console.log('id: ', id)
+
+        // submit analogy
+        const data = {
+            "topic": id,
+            "analogy": analogy.toString(),
+            "submittedBy": locals.pb.authStore.model?.id
+        }
+
+        try {
+            await locals.pb.collection('analogies').create(data)
+        } catch (error) {
+            console.log(error)
+            console.log('Something went wrong submitting the analogy to the server.')
+            return {
+                error: 'Something went wrong submitting the analogy to the server.'
+            }
+        }
+
+        return {
+            success: true
+        }
+    }
+};
--- analogy-quest/src/routes/new/+page.svelte
+++ analogy-quest/src/routes/new/+page.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+    import type { ActionData, PageData } from './$types';
     
-    // export let data: PageData;
+    export let data: PageData;
+    export let form: ActionData;
 </script>
 
 <section class="w-screen flex flex-col items-center justify-center space-y-4 py-8">
     <h1 class="text-4xl">New Analogy</h1>
+    <div class="min-h-[2em]">
+        {#if form?.error }
+            <p>{form?.error}</p>
+        {/if}
+        {#if form?.success }
+            <p>Your analogy was successfully submitted!</p>
+        {/if}
+    </div>
     <form method="post" class="flex flex-col px-20 space-y-2 w-full">
         <label for="topic">Topic</label>
         <input type="text" name="topic" />
 
-        <label for="topic">Analogy</label>
-        <textarea name="topic" placeholder="Enter your analogy here..." />
+        <label for="analogy">Analogy</label>
+        <textarea name="analogy" placeholder="Enter your analogy here..." />
         <button class="max-w-fit bg-green-400 p-2 rounded-md">Submit</button>
     </form>
 </section>
@@ -26,6 +35,6 @@
     }
 
     textarea {
-        @apply min-h-[35rem];
+        @apply min-h-[30rem];
     }
 </style>
